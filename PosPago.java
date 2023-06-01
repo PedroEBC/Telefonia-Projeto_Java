e5d7e5e7e556f0d63e74726b154112a349ba5ce0
Diego Sampaio <100983331+DiegoSampaio505@users.noreply.github.com>
2023-06-01 18:51:08 -0300

    Criando PosPago, adicionando constructor

---
+++ PosPago.java
@@ -0,0 +1,9 @@
+import java.util.GregorianCalendar;
+
+public class PosPago extends Assinante {
+    private float assinatura;
+
+    public PosPago(long cpf, String nome, int numero, float assinatura) {
+        super(cpf, nome, numero);
+        this.assinatura = assinatura;
+    }
