6228d67b6e2451c2a0ac03c28b794d11437d0fe7
Diego Sampaio <100983331+DiegoSampaio505@users.noreply.github.com>
2023-06-01 18:52:18 -0300

    Criando imprimirFatura PosPago

--- PosPago.java
+++ PosPago.java
@@ -19,3 +19,31 @@ public class PosPago extends Assinante {
     		System.out.println("\nVoce não tem mais chamadas");
     	}
     }
+
+     public void imprimirFatura (int mes) {
+    	System.out.println("\nFATURA");
+    	System.out.println("\nDados:");
+    	System.out.println("CPF: " + getCPF() + "\n" + toString());
+    	
+    	System.out.println("\nChamadas feitas no mês " + mes + ":");
+        for (int i = 0; i < numChamadas; i++) {
+            Chamada chamada = chamadas[i];
+            if (chamada.getData().get(GregorianCalendar.MONTH) + 1 == mes) {
+                System.out.println("\nData: " + chamada.getData().getTime());
+                System.out.println("Duração: " + chamada.getDuracao() + " minutos");
+                double custoChamada = chamada.getDuracao() * 1.04; // Custo por minuto
+                System.out.println("Custo: R$" + custoChamada);
+            }
+        }
+
+        double totalFatura = assinatura;
+        for (int i = 0; i < numChamadas; i++) {
+            Chamada chamada = chamadas[i];
+            if (chamada.getData().get(GregorianCalendar.MONTH) + 1 == mes) {
+                totalFatura += chamada.getDuracao() * 1.04;
+            }
+        }
+        System.out.println("\nValor total da fatura: R$" + totalFatura);
+    	
+    }
+}
